d9b878b179f9caf7faeafc6d1bc1d205563fea93
lajos papp <lalyos@yahoo.com>
2012-09-17 14:12:09 +0200

    introducing spring specific form:XXXX tags

--- src/main/java/com/acme/hooters/GirlsController.java
+++ src/main/java/com/acme/hooters/GirlsController.java
@@ -3,6 +3,7 @@ package com.acme.hooters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,12 +22,17 @@ public class GirlsController {
     @RequestMapping("list")
     public String list(Model model) {
         
-        
+        model.addAttribute("newGirl", new Girl());
+        model.addAttribute("basketOptions", getBasketOptions());
         model.addAttribute("girls", girlRepository.getAllGirls());
         
         return "girls/list";
     }
     
+    private Object getBasketOptions() {
+        return new String[]{"A","B","C","D"};
+    }
+
     @RequestMapping("remove")
     public String remove(@RequestParam String name, RedirectAttributes model) {
         
@@ -42,5 +48,11 @@ public class GirlsController {
         girlRepository.addGirl(newGirl);
         model.addFlashAttribute("msg", "You have succesfully added: " + newGirl.getName());
         return "redirect:/girls/list";        
+    }
+    
+    
+    @ExceptionHandler
+    public String errorPage(Exception e) {
+        return "error";
     }
 }
--- src/main/webapp/WEB-INF/views/girls/list.jsp
+++ src/main/webapp/WEB-INF/views/girls/list.jsp
@@ -1,4 +1,7 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form" %>
+
+<%@ taglib tagdir="/WEB-INF/tags" prefix="h" %>
 
 <c:if test="${not empty msg}">
   <div style="background: #050; border: 2px green solid; color:white; width: 300px;"> ${msg}
@@ -18,18 +21,16 @@
 
 </ul>
 
-<h2>Add Girl</h2>
-
+<h2>Add Girl v1.1</h2>
 <c:url var="addUrl" value="/girls/add" />
+<form:form action="${addUrl}" modelAttribute="newGirl">
+ <br/><form:input path="name"/>
+ <br/><form:input path="breastSize"/>
+ <br/><form:select path="basket" >
+ <form:options items="${basketOptions}"/>
+ </form:select>
+ <br/><form:button>add</form:button>
+</form:form>
 
-<form action="${addUrl}" method="post">
- <br>Name: <input type="text" name="name" />
- <br>Breast Size: <input type="text" name="breastSize" />
- <br>Basket type: <select name="basket">
- <option value="A" >A</option>
- <option value="B" >B</option>
- <option value="C" >C</option>
- <option value="D" >D</option>
-</select>
-<br/><input type="submit" value="add"/>
-</form>
+
+<h:debugMap map="${sessionScope}" title="session"></h:debugMap>
